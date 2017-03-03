71f160d416327ccf91e154834cb4398eed799dea
BaranovMykola <mapsg32@gmail.com>
2017-03-03 16:06:43 +0200

    implemented writing to console by QProccess::write(). Should be used pr.wait...()

--- main.cpp
+++ main.cpp
@@ -11,21 +11,49 @@ using std::cout;
 using std::boolalpha;
 using std::endl;
 
+void proccesState(QProcess* pr)
+{
+    QString str;
+    QProcess::ProcessState state = pr->state();
+    switch (state) {
+    case QProcess::NotRunning:
+        str = "Not running";
+        break;
+    case QProcess::Running:
+        str = "Running";
+        break;
+    case QProcess::Starting:
+        str = "Starting";
+        break;
+    }
+    qDebug() << "QProccesState: " << str;
+}
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
-    QString gdb = qApp->applicationDirPath().append("/gdb/test.exe");
+    QString gdb = qApp->applicationDirPath().append("/gdb/a2.exe");
     QFile gdbFile(gdb);
     qDebug() << "gdb path: " << gdb;
     qDebug() << "gdb file: " << gdbFile.exists();
 
-    QProcess* dbProcces = new QProcess();
-    //dbProcces->start(gdb, QStringList() << "");
-    dbProcces->setProgram(gdb);
-    dbProcces->execute(gdb, QStringList() << "111");
-    QString firstRead = dbProcces->readAllStandardOutput();
+    QProcess pr;
+    proccesState(&pr);
+    pr.start(gdb);
+    pr.waitForBytesWritten();
+    qDebug() << "write ready";
+    pr.write("5\n");
+    pr.waitForFinished();
+    qDebug() << pr.readAllStandardOutput();
+//    dbProcces->start(gdb, QStringList() << "arg");
 
-    qDebug() << "firstRead: " << firstRead;
 
-    return a.exec();
+//    QString firstRead = dbProcces->readAllStandardOutput();
+//    proccesState(dbProcces);
+//    qDebug() << "firstRead: " << firstRead;
+
+//    dbProcces->write("s");
+//    proccesState(dbProcces);
+    qDebug() << "*Finished main*";
+    return 0;
 }
